982039d1457d706ac0c0a03031c8a26099ed73cf
Marcel Giemza <marcel.giemza@outlook.com>
2024-11-27 10:01:01 +0000

    e

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,20 @@
+use std::cell::RefCell;
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
+}
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new())
+}
+
+#[ic_cdk::query]
+fn save_msg(msg: String) {
+    MSG.with(|static_msg| *static_msg.borrow_mut() = msg);
+}
+
+#[ic_cdk::query]
+fn get_msg() -> String {
+    MSG.with(|static_msg| static_msg.borrow().clone())
 }
